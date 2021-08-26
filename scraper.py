a46576e6521f0497d2e1281eb81b60e7a22bb959
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-08-25 18:47:51 -0700

    finished lab, successfully scraped wiki for citations needed, prints count and parent report

---
+++ scraper.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+# -------------------- Address To Be Scraped ----------------- # 
+URL = 'https://en.wikipedia.org/wiki/History_of_Mexico'
+# ------------ Main Function ----------- # 
+def scraper_wiki_citations_needed(URL):
+    print(get_citations_needed_count(URL))
+    print(get_citations_needed_report(URL))
+
+# Would prefer a function that gets soup independently of count/report
+# def get_soup(URL):
+#     page = requests.get(URL)
+#     # print(page.content)
+#     soup = BeautifulSoup(page.content, 'html.parser')
+#     return soup.prettify()
+
+
+# -------------------- Count Function -------------------- # 
+def get_citations_needed_count(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(class_='noprint Inline-Template Template-Fact')
+    print(len(results))
+
+# -------------------- Report Function -------------------- # 
+def get_citations_needed_report(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(class_='noprint Inline-Template Template-Fact')
+    print(results.prettify())
+    for item in results:
+        print(item.parent.parent.text)
+
+# ------ Function Call ------- #
+scraper_wiki_citations_needed(URL)
